1034ad3da36b203630aba9b5941b4b3e7989feac
Ariel <arielalejandrorojas@gmail.com>
2023-01-07 15:13:23 -0700

    just getting use to

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const routes = require('express').Router();
-routes.get('/',(req,res)=>{
-    res.send('Ariel Rojas');
+
+routes.get('/', (req, res) => {
+    res.send('<h1>Ariel Rojas</h1>');
+
 })
+
+routes.get('/test', (req, res) => {
+    res.send('<h1>Ariel Rojas</h1>');
+
+})
+
 module.exports = routes;
